2fe4f7344c0d4ed63d8f65700a7be1cb514e948a
fEdGoRv <vilatte612@gmail.com>
2022-09-17 13:57:57 +0200

    primera entrega componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NavbarComponent } from 'src/app/components/navbar/navbar.component';
 
 import { AppComponent } from './app.component';
+import { StudentComponent } from './components/student/student.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ToolbarComponent,
+    StudentComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent,
+    NavbarComponent,
+    ToolbarComponent,
+    StudentComponent
+  ]
 })
 export class AppModule { }
